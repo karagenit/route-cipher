e9bd2ff7ba9ee7ebbd247758d7cca3e6c7a8216c
Caleb Smith <karagenit@outlook.com>
2018-06-10 15:41:54 -0400

    Finish String Cleaning

--- cipher.c
+++ cipher.c
@@ -1,28 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Removes spaces etc, makes all caps, pads with Xes
+// Prepares string for cipher
 char* prepString(char *str, int targetLength) {
-    // Count # of letters in str
-    int stripSize = 0;
-    char c;
-    for (int i = 0; (c = str[i]) != 0; i++) {
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
-            stripSize++;
-        }
-    }
-    // Allocate string for them only
-    char *stripStr = calloc(stripSize, sizeof(char)); //FREEME
+    // Allocate memory for cleaned string
+    char *stripStr = calloc(targetLength, sizeof(char)); //FREEME
     int stripIndex = 0;
+    char c;
     // Copy only letters from string into new string
     for (int i = 0; (c = str[i]) != 0; i++) {
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+        // Convert lower case to upper case
+        if (c >= 'a' && c <= 'z') {
+            c = c - 'a' + 'A';
+        }
+        // Save only text characters
+        if (c >= 'A' & c <= 'Z') {
             stripStr[stripIndex] = c;
             stripIndex++;
-            putchar(c);
         }
     }
-    // Convert text into all caps
+    // Pad remaining space with Xes
+    for (int i = stripIndex; i < targetLength; i++) {
+        stripStr[i] = 'X';
+    }
 }
 
 char* cwRoute(char *str, int cols, int rows) {
