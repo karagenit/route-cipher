0c97932ade7cb0d2e71cc47a0f326c451d09a093
Caleb Smith <karagenit@outlook.com>
2018-06-10 16:15:50 -0400

    Add Route Function, Fix Box Index Bug

--- cipher.c
+++ cipher.c
@@ -34,25 +34,26 @@ char* cwRoute(char *str, int cols, int rows) {
     int cipherIndex = 0;
     int stringIndex = cols - 1;
     // Iterates over each 'box'
-    for (int ci = cols, cr = rows; ci > 0 && cr > 0; ci -= 2, cr -= 2) {
+    for (int boxCols = cols, boxRows = rows; boxCols > 0 && boxRows > 0; 
+            boxCols -= 2, boxRows -= 2) {
         cipher[cipherIndex] = string[stringIndex];
         cipherIndex += 1;
-        for (int i = 0; i < rows - 1; i++) {
+        for (int i = 0; i < boxRows - 1; i++) {
             stringIndex += cols;
             cipher[cipherIndex] = string[stringIndex];
             cipherIndex += 1;
         }
-        for (int i = 0; i < cols - 1; i++) {
+        for (int i = 0; i < boxCols - 1; i++) {
             stringIndex -= 1;
             cipher[cipherIndex] = string[stringIndex];
             cipherIndex += 1;
         }
-        for (int i = 0; i < rows - 1; i++) {
+        for (int i = 0; i < boxRows - 1; i++) {
             stringIndex -= cols;
             cipher[cipherIndex] = string[stringIndex];
             cipherIndex += 1;
         }
-        for (int i = 0; i < cols - 2; i++) {
+        for (int i = 0; i < boxCols - 2; i++) {
             stringIndex += 1;
             cipher[cipherIndex] = string[stringIndex];
             cipherIndex += 1;
@@ -64,9 +65,13 @@ char* cwRoute(char *str, int cols, int rows) {
     return cipher;
 }
 
-void main() {
-    char *text = cwRoute("Hello!", 3, 2);
-    printf("%.*s", 6, text);
+void route(char *str, int cols, int rows) { // TODO: cw vs ccw
+    char *text = cwRoute(str, cols, rows);
+    printf("%.*s", cols * rows, text);
     free(text);
+}
+
+void main() {
+    route("We are discovered, flee at once", 9, 3);
 }
 
